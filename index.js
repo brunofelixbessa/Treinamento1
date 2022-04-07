d5729f1d6792cb68c250c468ab4eac3cee8352a0
brunofelixbessa <brunofelixbessa@gmail.com>
2022-04-07 18:20:43 -0300

    Meu primeiro commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+let texto1 = document.getElementById('b1');
+let email = document.getElementById('b2');
+
+// metoodo para mostrar um alerta de treinamento 
+function Vaitimao() {
+    alert('Formulário enviado com sucesso!' + texto1.value + '<br/>' + email.value);
+}
